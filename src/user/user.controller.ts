18a988146bf65321fb03813d7af7ce0edb3b8063
Mahdi-Ettehadnejad <mahdi.ettehad85@gmail.com>
2024-04-28 07:27:35 +0330

    feat(auth): register complete, token generated

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '@/user/user.service';
 import { CreateUserDto } from '@/user/dto/createUser.dto';
+import { UserResponseInterface } from './types/userResponse.interface';
 
 @Controller()
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('users')
-  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<any> {
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserResponseInterface> {
     const user = await this.userService.createUser(createUserDto);
     return this.userService.buildUserResponse(user);
   }
